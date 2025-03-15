7ec5a9abc19dec7fdd2424e3e61ff087b9cd063e
Max Drob <maxdrobs@gmail.com>
2025-03-16 00:55:50 +0300

    First commit

---
+++ python_training/task_8.1_if_elif_else.py
@@ -0,0 +1,33 @@
+# num_float = 3.4
+#
+# if num_float > 0:
+#     print("positive number")
+# elif num_float == 0:
+#     print("Zero")
+# else:
+#     print("negative number")
+
+
+# permit_print = True
+#
+# if num > 0 and permit_print:
+#     print("num is a positive number")
+# elif not permit_print:
+#     print("printing prohibited")
+
+# def student_rank(year_of_study):
+#     if 1 >= year_of_study <= 4:
+#         print("bachelor")
+#     elif year_of_study in range(5, 7):
+#         print("master")
+#     elif 7 <= year_of_study <= 9:
+#         print("postgraduate")
+#     else:
+#         print("enter the correct year of study")
+#
+# student_rank(7)
+
+num = int(-1000)
+if num <-100 or num>100:
+    print('-')
+else: print('+')
