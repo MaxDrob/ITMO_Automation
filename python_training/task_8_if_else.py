66a8b4fba25574947147fae950d17be3c744e647
Max Drob <maxdrobs@gmail.com>
2025-03-16 00:24:23 +0300

    First commit

--- python_training/task_8_if_else.py
+++ python_training/task_8_if_else.py
@@ -1,8 +1,16 @@
 #num = 3
-num = -5
+#num = -5
 #num = 0
 
-if num >= 0:
-    print("Number is equal or greater than 0")
-else:
-    print("The number is negative")
+# if num >= 0:
+#     print("Number is equal or greater than 0")
+# else:
+#     print("The number is negative")
+
+def task_yes_no(str_1, str_2):
+    if str_1 in str_2:
+        print("YES")
+    else:
+        print("NO")
+
+task_yes_no(str_1="tet", str_2="test1'")
